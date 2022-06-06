58c9ee81712f2925b53b68c4b43114b5c56775ab
Marzena Halama <mhalama@iitis.pl>
2022-06-06 17:25:50 +0200

    add min function

--- Ising.py
+++ Ising.py
@@ -3,25 +3,32 @@ from sympy.utilities.iterables import multiset_permutations
 
 
 sum_energy = []
-n=5
-v = np.ones((n), dtype=int)
-x = np.ones((n), dtype=int)
+n = 6
+interaction = np.ones((n), dtype=int)
+set_of_configuration = np.ones((n), dtype=int)
+temp_min_energy = 30.0
+temp_set = np.array([])
 
-for i in range(len(x)+1):
-    if i>0: x[i-1] = -1
-    for p in multiset_permutations(x):
+
+for i in range(n+1):
+    if i>0: set_of_configuration[i-1] = -1
+    for p in multiset_permutations(set_of_configuration):
         print(p)
         su = 0.0
         for d in range(len(p)):
             if d < len(p)-1:
-                su += p[d]*p[d+1]*v[d]
+                su += p[d]*p[d+1]*interaction[d]
                 # print(p[d], p[d+1])
             else:
-                su += p[0]*p[-1]*v[-1]
+                su += p[0]*p[-1]*interaction[-1]
                 # print(p[0], p[-1])
+        if temp_min_energy > su:
+            temp_min_energy = su 
+            temp_set = p
+        
         sum_energy.append(su)
+    
 
 print(sum_energy)
-
-
+print(temp_min_energy, temp_set)
     
